47a17488cb238c6e523fbd17cd125d76dd69a294
Ishika Soni <ishika@iitk.ac.in>
2016-11-09 01:33:51 +0530

    Added User page and redirect to their homepage

--- log/urls.py
+++ log/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^$',views.homepage, name='homepage'),
     url(r'^review$', views.review_list, name='review_list'),
     url(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
+    url(r'^user/(?P<user_name>[a-z]+)/$', views.user_detail, name='user_detail'),
     url(r'^course$', views.course_list, name='course_list'),
     url(r'^course/(?P<course_id>[0-9]+)/$', views.course_detail, name='course_detail'),
     url(r'^course/(?P<course_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
