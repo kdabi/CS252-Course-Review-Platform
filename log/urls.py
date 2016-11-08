a902717da9381e453fb771ee5978550e48017c4e
Kritagya <kdabi@iitk.ac.in>
2016-11-09 01:21:50 +0530

    added faculty list

--- log/urls.py
+++ log/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^review$', views.review_list, name='review_list'),
     url(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
     url(r'^course$', views.course_list, name='course_list'),
+    url(r'^faculty$', views.faculty_list, name='faculty_list'),
     url(r'^course/(?P<course_id>[0-9]+)/$', views.course_detail, name='course_detail'),
     url(r'^course/(?P<course_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
     url(r'^user_login$', views.user_login, name='auth'),
